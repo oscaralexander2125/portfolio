1d1ce5b86f69fe60a3380b6271f80e90084ebfce
Oscar A. Reyes <oscarreyes2194@gmail.com>
2019-05-09 19:02:10 -0500

    added links

---
+++ index.js
@@ -0,0 +1,15 @@
+function runApp() {
+  $('.app-screenshot-1').click(function() {
+    window.open('https://intense-river-44339.herokuapp.com/sign-in', '_blank')
+  })
+  
+  $('.app-screenshot-2').click(function() {
+    window.open('https://warm-bayou-57425.herokuapp.com/', '_blank')
+  })
+  
+  $('.app-screenshot-3').click(function() {
+    window.open('https://oscaralexander2125.github.io/new-API-capstone/', '_blank')
+  })
+}
+
+$(runApp)
