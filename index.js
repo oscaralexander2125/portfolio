136a89f898c44217da354e5eb99e5251b09e1577
Oscar A. Reyes <oscarreyes2194@gmail.com>
2022-05-07 20:59:45 -0500

    added web app link

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function runApp() {
   $(".app-screenshot-1").click(function () {
-    window.open("https://marvelous-tulumba-d99744.netlify.app/", "_blank");
+    window.open("https://aquamarine-stardust-fe34f5.netlify.app/", "_blank");
   });
 
   $(".app-screenshot-2").click(function () {
