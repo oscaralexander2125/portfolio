1d1cc4f5493b3134acd83dd671cf17acd285d3c9
Oscar A. Reyes <oscarreyes2194@gmail.com>
2022-05-07 20:43:44 -0500

    added relevant link

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 function runApp() {
-  $('.app-screenshot-1').click(function() {
-    window.open('https://marvelous-tulumba-d99744.netlify.app/', '_blank')
-  })
-  
-  $('.app-screenshot-2').click(function() {
-    window.open('https://warm-bayou-57425.herokuapp.com/', '_blank')
-  })
-  
-  $('.app-screenshot-3').click(function() {
-    window.open('https://oscaralexander2125.github.io/new-API-capstone/', '_blank')
-  })
+  $(".app-screenshot-1").click(function () {
+    window.open("https://marvelous-tulumba-d99744.netlify.app/", "_blank");
+  });
+
+  $(".app-screenshot-2").click(function () {
+    window.open("https://marvelous-tulumba-d99744.netlify.app/", "_blank");
+  });
 }
 
-$(runApp)
+$(runApp);
